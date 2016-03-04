8bbc9f3ebb7af4ecd95be97e3d2e6ca9a9b7f430
vasigoal <vasigoal@gmail.com>
2016-03-04 18:15:39 +0100

    Added timeline

--- js/countdown.js
+++ js/countdown.js
@@ -13,26 +13,30 @@
 		}
 		//main countdown function
 		function countdown_proc() {
-			eventDate = Date.parse('Apr 1, 2016') / 1000;
+			eventDate = Date.parse('Nov 17, 1983') / 1000;
 			currentDate = Math.floor($.now() / 1000);
-			if(eventDate <= currentDate) {
+			if(eventDate >= currentDate) {
 				callback.call(this);
 				clearInterval(interval);
 			}
-			seconds = eventDate - currentDate;
-			days = Math.floor(seconds / (60 * 60 * 24)); //calculate the number of days
-			seconds -= days * 60 * 60 * 24; //update the seconds variable with no. of days removed
+			seconds = currentDate - eventDate;
+			years = Math.floor(seconds / (60 * 60 * 24 * 365)); //calculate the number of days
+			seconds -= years * 60 * 60 * 24 * 365; //update the seconds variable with no. of days removed
+			days = Math.floor(seconds / (60 * 60 * 24));
+			seconds -= days * 60 * 60 * 24; //update the seconds variable with no. of hours removed
 			hours = Math.floor(seconds / (60 * 60));
-			seconds -= hours * 60 * 60; //update the seconds variable with no. of hours removed
+			seconds -= hours * 60 * 60; //update the seconds variable with no. of minutes removed
 			minutes = Math.floor(seconds / 60);
-			seconds -= minutes * 60; //update the seconds variable with no. of minutes removed
+
 			//conditional Ss
-			if (days == 1) { thisEl.find(".timeRefDays").text("day"); } else { thisEl.find(".timeRefDays").text("days"); }
-			if (hours == 1) { thisEl.find(".timeRefHours").text("hour"); } else { thisEl.find(".timeRefHours").text("hours"); }
-			if (minutes == 1) { thisEl.find(".timeRefMinutes").text("minute"); } else { thisEl.find(".timeRefMinutes").text("minutes"); }
+			if (years == 1) { thisEl.find(".timeRefDays").text("year"); } else { thisEl.find(".timeRefDays").text("years"); }
+			if (days == 1) { thisEl.find(".timeRefHours").text("day"); } else { thisEl.find(".timeRefHours").text("days"); }
+			if (hours == 1) { thisEl.find(".timeRefMinutes").text("hour"); } else { thisEl.find(".timeRefMinutes").text("hours"); }
+			if (minutes == 1) { thisEl.find(".timeRefSeconds").text("minute"); } else { thisEl.find(".timeRefSeconds").text("minutes"); }
 			if (seconds == 1) { thisEl.find(".timeRefSeconds").text("second"); } else { thisEl.find(".timeRefSeconds").text("seconds"); }
 			//logic for the two_digits ON setting
 			if(options['format'] == "on") {
+				years = (String(years).length >= 2) ? years : "0" + years;
 				days = (String(days).length >= 2) ? days : "0" + days;
 				hours = (String(hours).length >= 2) ? hours : "0" + hours;
 				minutes = (String(minutes).length >= 2) ? minutes : "0" + minutes;
@@ -40,6 +44,7 @@
 			}
 			//update the countdown's html values.
 			if(!isNaN(eventDate)) {
+				thisEl.find(".years").text(years);
 				thisEl.find(".days").text(days);
 				thisEl.find(".hours").text(hours);
 				thisEl.find(".minutes").text(minutes);
